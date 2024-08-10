5e39794ac6f09ee3a86718660876f5334a3b3b3f
b22marah <b22marah@student.his.se>
2024-08-10 12:56:18 +0200

    Added ts npm package

---
+++ script.ts
@@ -0,0 +1,11 @@
+const person ={
+    "name": "John Doe",
+    "age": 30,
+    "isStudent": false,
+    "hobbies": ["reading", "coding", "playing guitar"],
+    "address": {
+        "street": "123 Main St",
+        "city": "New York",
+        "country": "USA"
+    }
+};
